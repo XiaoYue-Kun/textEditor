0c26044233d5cc87c17dc63a715d968937d14f5f
XiaoYue-Kun <junchencai1@gmail.com>
2025-03-29 21:05:05 -0500

    Initial commit: Basic viewing and editing functions

---
+++ editor.h
@@ -0,0 +1,58 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+enum editorkey{
+    BACKSPACE = 127,
+    ARROW_LEFT = 512,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
+};
+
+typedef struct abuf {
+    char *b;
+    int len;
+} abuf;
+
+typedef struct erow{
+    int size;
+    int rsize;
+    char *chars;
+    char *render;
+} erow;
+
+struct editorConfig {
+    int cx, cy;
+    int rx;
+    int rowoff;
+    int coloff;
+    int endmoving;
+    int prevcol;
+    int screenrows;
+    int screencols;
+    int tabsize;
+    int numrows;
+    erow *row;
+    int dirty;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
+    struct termios orig_termios;
+} E;
+
+void editorSetStatusMessage(const char *fmt, ...);
